d7ac8f4a580d157f8ea1cd76c09be365f0b62063
Jon Leopard <jontron2000@gmail.com>
2018-09-25 19:15:26 +0200

    Add babel plugin for styled components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { configure, addDecorator } from '@storybook/react'
-import { createGlobalStyle } from 'styled-components'
-import { ThemeProvider, Box } from '../src'
-import 'storybook-chromatic';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { createGlobalStyle } from "styled-components";
+import { ThemeProvider, Box } from "../src";
+import "storybook-chromatic";
 
 const GlobalStyle = createGlobalStyle([], {
-  '*': {
-    boxSizing: 'border-box'
+  "*": {
+    boxSizing: "border-box"
   },
   body: {
     lineHeight: 1.5,
     margin: 0
   }
-})
+});
 
 addDecorator(story => (
   <ThemeProvider>
     <Box p={3}>{story()}</Box>
   </ThemeProvider>
-))
+));
 
-const req = require.context('.', true, /\.js$/)
+const req = require.context(".", true, /\.js$/);
 
 const load = () => {
-  req.keys().forEach(req)
-}
+  req.keys().forEach(req);
+};
 
-configure(load, module)
+configure(load, module);
